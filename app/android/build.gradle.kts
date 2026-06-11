5ef6ed6138c981bbd4776ee99c59ee621edfc192
최재관 <onlyvideo925@gmail.com>
2026-06-12 04:17:03 +0900

    chore(android): suppress java 8 obsolete warnings (-Xlint:-options)

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -19,6 +19,13 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 서드파티 플러그인 Java 8 타깃 경고 억제 (빌드 영향 없는 로그 소음)
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
